36679984c7f3015bf575bc036004b50b3e0b43c9
Prem Kumar <premkumar5012002@gmail.com>
2024-03-12 11:04:31 +0530

    Updated Biography

--- src/components/Apps/Biography/index.tsx
+++ src/components/Apps/Biography/index.tsx
@@ -24,37 +24,53 @@ export const Biography: FC = () => {
           <Icon icon="logos:docker-icon" className="h-6 w-6" />
         </div>
 
+        <div className="mt-6 border-2  border-white border-b-black border-r-black bg-accent p-4 font-sans text-white">
+          <p>
+            I&apos;m actively seeking a full stack developer position. If your
+            team needs someone with experience in React, Next.js, or backend
+            technologies like REST or GraphQL, feel free to reach out to me via
+            email at{" "}
+            <a
+              target="_blank"
+              className="font-bold underline"
+              href="mailto:premkumar5012002@gmail.com"
+            >
+              premkumar5012002@gmail.com
+            </a>{" "}
+          </p>
+        </div>
+
         <div className="pt-8">
           <h3 className="text-xl font-bold">About Me</h3>
           <p className="pt-3">
-            Junior Software Engineer based in Chennai. Strong skills in
-            Javascript, Typescript and Dart through experience building full
-            stack applications using React and Flutter.
+            As a Full Stack Software Developer, I specialize in Frontend
+            Development using React and backend technologies like Rest and
+            GraphQL APIs. My focus is on delivering exceptional user experiences
+            and optimizing performance through innovative solutions.
           </p>
         </div>
 
         <div className="pt-8">
           <h3 className="text-xl font-bold">Web Developer</h3>
           <p className="pt-3">
-            As a web developer, I have also had a lot of chances to practice and
-            participate in projects to further hone my skills. I have plenty of
-            experience with backend as well as databases using SQL or NoSQL
-            solutions. But most of my time is dedicated to frontend development
-            using react.js and elaborate styling of websites (such as this one
-            😁).
+            As a web developer, I've honed my skills through various projects,
+            with extensive experience in backend development and database
+            management using SQL or NoSQL solutions. My primary focus lies in
+            frontend development using React.js, alongside meticulous styling to
+            enhance user interfaces.
           </p>
         </div>
 
         <div className="pt-8">
           <h3 className="text-xl font-bold">Mobile Developer</h3>
           <p className="pt-3">
-            As a mobile developer, I have also had a lot of chances to build
-            mobile applictions using flutter both for personal and freelance
-            projects. You can check out my projects{" "}
+            As a mobile developer, I specialize in creating mobile applications
+            using Flutter, both for personal and freelance projects. You can
+            explore examples of my work, such as{" "}
             <a
               target="_blank"
               href="https://play.google.com/store/apps/details?id=com.appmakerss.costfocus&hl=en&gl=US"
-              className="text-accent underline"
+              className="font-bold text-accent underline"
             >
               Costfocus
             </a>{" "}
@@ -62,11 +78,11 @@ export const Biography: FC = () => {
             <a
               target="_blank"
               href="https://play.google.com/store/apps/details?id=com.bitsandbytes.learntogether&hl=en&gl=IN"
-              className="text-accent underline"
+              className="font-bold text-accent underline"
             >
               Tutrpro
             </a>{" "}
-            on Google play store.
+            on the Google Play Store.
           </p>
         </div>
 
